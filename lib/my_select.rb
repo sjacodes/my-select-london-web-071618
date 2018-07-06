780d5bcfdfd3cebb60c7ab80dd54988c373377b5
Sarah Jacob <smjacob91@gmail.com>
2018-07-06 10:20:07 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,10 +4,10 @@ def my_select(collection)
     i = 0
     while i < collection.length
       if (yield collection[i]) == true
-      
+        selected_out_of_collection.push(collection[i])
+        i = i + 1
+        
   while i<collection.length 
-if yield(collection[i])==true
-  new_array.push(collection[i])
 end
 i+=1 
 end
