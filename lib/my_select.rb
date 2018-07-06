d47b4f3bb49109e54541b884dc52810d65c971ed
Sarah Jacob <smjacob91@gmail.com>
2018-07-06 10:26:06 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    selected_out_of_collection = []
+    i = 0
+    while i < collection.length
+      if (yield collection[i]) == true
+        selected_out_of_collection << collection[i]
+      end
+      i = i + 1
+    end
+  selected_out_of_collection
+  end
 end
+
